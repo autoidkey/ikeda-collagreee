42f70fb29ed5bf9fac54b5b24469cd7f0341a2df
ganmacs <ganmacs@gmail.com>
2014-08-25 12:31:06 +0900

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: 'comments#index'
+  root to: 'home#index'
 
   resource :comments
   resource :activities
