c2e24a0a13fd5d6e725afb6d0c0077e4a7b62033
ganmacs <ganmacs@gmail.com>
2014-08-26 18:25:57 +0900

    chane view and scheme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824161548) do
+ActiveRecord::Schema.define(version: 20140826041933) do
 
   create_table "activities", force: true do |t|
     t.string   "info"
@@ -22,33 +22,74 @@ ActiveRecord::Schema.define(version: 20140824161548) do
     t.datetime "updated_at"
   end
 
-  create_table "comments", force: true do |t|
-    t.string   "content",      default: ""
+  create_table "entries", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "parent_id"
     t.integer  "np",           default: 0
-    t.boolean  "root_node",    default: false
-    t.string   "form",         default: ""
+    t.integer  "user_id"
     t.boolean  "facilitation", default: false
+    t.boolean  "invisible",    default: false
+    t.boolean  "top_fix",      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",                      null: false
+  end
+
+  create_table "exclusions", force: true do |t|
+    t.string   "word"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "issues", force: true do |t|
+    t.string   "name"
+    t.integer  "theme_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "keywords", force: true do |t|
+    t.string   "word"
+    t.float    "score",      limit: 24
+    t.integer  "agree"
+    t.integer  "disagree"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "themes", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.string   "color"
+    t.boolean  "facilitation", default: false
+    t.boolean  "nolink",       default: false
+    t.boolean  "secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",                            null: false
-    t.string   "realname",                            null: false
+    t.string   "realname",                              null: false
     t.integer  "role",                   default: 1
+    t.string   "name",                                  null: false
+    t.integer  "gender"
+    t.integer  "age"
+    t.string   "home"
+    t.string   "move"
+    t.boolean  "remind",                 default: true
+    t.integer  "mail_format",            default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
