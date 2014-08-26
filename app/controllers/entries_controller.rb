b6e4e16d8f875da76329e35dc20126b58d9a7691
ganmacs <ganmacs@gmail.com>
2014-08-26 22:20:30 +0900

    fix indent

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,40 +1,40 @@
  class EntriesController < ApplicationController
-  before_action :set_entry, only: %i(show)
+   before_action :set_entry, only: %i(show)
 
-  def index
-    @entries = Entry.all
-  end
+   def index
+     @entries = Entry.all
+   end
 
-  def show
-    @new_entry = Entry.new { |obj| obj.parent_id = @entry.id }
-    @entries = Entry.where(parent_id: @entry.id)
-  end
+   def show
+     @new_entry = Entry.new { |obj| obj.parent_id = @entry.id }
+     @entries = Entry.where(parent_id: @entry.id)
+   end
 
-  def new
-    @entry = Entry.new
-  end
+   def new
+     @entry = Entry.new
+   end
 
-  def create
-    @entry = Entry.new(entry_params)
+   def create
+     @entry = Entry.new(entry_params)
 
-    respond_to do |format|
-      if @entry.save
-        format.html { redirect_to theme_path(@entry.theme), notice: '投稿しました' }
-        format.json { render 'show', status: :created, location: @entry }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+     respond_to do |format|
+       if @entry.save
+         format.html { redirect_to theme_path(@entry.theme), notice: '投稿しました' }
+         format.json { render 'show', status: :created, location: @entry }
+       else
+         format.html { render action: 'new' }
+         format.json { render json: @entry.errors, status: :unprocessable_entity }
+       end
+     end
+   end
 
-  private
+   private
 
-  def set_entry
-    @entry = Entry.find(params[:id])
-  end
+   def set_entry
+     @entry = Entry.find(params[:id])
+   end
 
-  def entry_params
-    params.require(:entry).permit(:title, :body, :user_id, :parent_id, :np, :theme_id)
-  end
-end
+   def entry_params
+     params.require(:entry).permit(:title, :body, :user_id, :parent_id, :np, :theme_id)
+   end
+ end
