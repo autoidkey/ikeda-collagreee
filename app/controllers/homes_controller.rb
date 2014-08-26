211614b91e07fe5b42958f72a8bd48ccdc3faad4
ganmacs <ganmacs@gmail.com>
2014-08-26 18:17:04 +0900

    change home_controller to homes_controller because of typo

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,25 @@
+class HomesController < ApplicationController
+  def collagree
+  end
+
+  def about
+  end
+
+  def admin
+  end
+
+  def statistic
+  end
+
+  def introduction
+  end
+
+  def privacy
+  end
+
+  def agreement
+  end
+
+  def project
+  end
+end
