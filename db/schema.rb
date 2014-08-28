be81bff5b0dded1bfdc0a6de0da596990d3b1390
ganmacs <ganmacs@gmail.com>
2014-08-29 00:17:39 +0900

    chafe default value role in user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828125348) do
+ActiveRecord::Schema.define(version: 20140828151448) do
 
   create_table "activities", force: true do |t|
     t.integer  "atype"
