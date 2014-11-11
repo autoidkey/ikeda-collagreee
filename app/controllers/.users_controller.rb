8862979e10808c540c45cc5296f495cc6ade5299
Yuma Imi <yuma.imi13@gmail.com>
2014-11-11 11:12:54 +0900

    ログイン・ログアウト時にUserのimageがupdateしてしまうバグ修正（image_uploader.rbのfilenameがぶつかっている模様）

---
+++ app/controllers/.users_controller.rb
@@ -0,0 +1,17 @@
+# class UsersController < ApplicationController
+#   # point_api
+#   def entry_point
+#     data = { point: current_user.entry_point(params[:theme_id]) }
+#     render json: data.to_json
+#   end
+
+#   def sum_point
+#     data = { point: current_user.sum_point(params[:theme_id]) }
+#     render json: data.to_json
+#   end
+
+#   def liked_point
+#     data = { point: current_user.liked_point(params[:theme_id]) }
+#     render json: data.to_json
+#   end
+# end
