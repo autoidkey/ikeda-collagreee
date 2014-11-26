c25759278717bf929665805f1661235c8f1979e5
Yuma Imi <yuma.imi13@gmail.com>
2014-11-26 19:29:05 +0900

    [modify]画像表示に関してサイズやリンク等編集
    議論ツリーに適切なサイズに変更。モーダルにする必要がある

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process :resize_to_fit => [300, 210]
+  process :resize_to_fit => [1000, 1000]
 
   # Create different versions of your uploaded files:
   version :thumb do
