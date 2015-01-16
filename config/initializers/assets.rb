4aa39b8b1d9daa34d0eb5a0a94de6b1e696549bf
13 <yuma.imi13@gmail.com>
2015-01-16 18:37:59 +0900

    [add]mobile.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile +=
-  ['pnotify.custom.min.js', 'no_point_function.css']
+  ['pnotify.custom.min.js', 'no_point_function.css', 'mobile.css']
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
