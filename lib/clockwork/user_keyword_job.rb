9d1439c571f567df91d75a15b832675b1934f809
13 <yuma.imi13@gmail.com>
2015-01-27 11:27:15 +0900

    test

--- lib/clockwork/user_keyword_job.rb
+++ lib/clockwork/user_keyword_job.rb
@@ -6,24 +6,24 @@ class UserKeywordJob
       user.keywords.delete_all
 
       user.joins.each do |join|
-        p user
-        next if user.joins.blank?
+        unless user.joins.blank?
+          p user
+          entries = join.theme.entries.where(user_id: user)
+          replies = entries.map(&:parent)
+          likes = Like.where(user_id: user, theme_id: join.theme.id).map(&:entry)
+          text = entries + replies + likes
 
-        entries = join.theme.entries.where(user_id: user)
-        replies = entries.map(&:parent)
-        likes = Like.where(user_id: user, theme_id: join.theme.id).map(&:entry)
-        text = entries + replies + likes
-
-        calculate(text).each do |key, val|
-          params = {
-            word: key,
-            score: val[:score],
-            agree: val[:agree],
-            disagree: val[:disagree],
-            theme_id: join.theme.id,
-            user_id: user.id
-          }
-          Keyword.new(params).save
+          calculate(text).each do |key, val|
+            params = {
+              word: key,
+              score: val[:score],
+              agree: val[:agree],
+              disagree: val[:disagree],
+              theme_id: join.theme.id,
+              user_id: user.id
+            }
+            Keyword.new(params).save
+          end
         end
       end
     end
