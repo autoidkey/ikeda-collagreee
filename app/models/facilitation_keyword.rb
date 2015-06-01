d09e1671e9c36c3bcc34a35c0d19de908669e2b8
takahashi.kazumasa <takahashi.kazumasa@itolab.nitech.ac.jp>
2015-06-01 15:13:22 +0900

    [modify]facilitation_keywordsモデルにバリデーションを追加

--- app/models/facilitation_keyword.rb
+++ app/models/facilitation_keyword.rb
@@ -1,2 +1,8 @@
 class FacilitationKeyword < ActiveRecord::Base
+	validates :theme_id, presence: true
+	validates :word, presence: true
+	validates :score, presence: true
+	validates :score, numericality: {
+		greater_than: 0, less_than: 1
+	}
 end
