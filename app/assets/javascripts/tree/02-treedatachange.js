483dc5c02f1ac2648e776766d08cf563970d06f2
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-07-05 21:26:12 +0900

    議論ツリーの修正

--- app/assets/javascripts/tree/02-treedatachange.js
+++ app/assets/javascripts/tree/02-treedatachange.js
@@ -1,31 +1,27 @@
 function tree_data_change(dataAll,classes){
 	var change_array = dataAll
 	var class_array = []
-	var len = dataAll.length;
-	console.log(change_array);
+	var len = max_id(dataAll);
+	var datalen = dataAll.length
 	for (var i = 0; i < classes.length; i++){
 		var push_array = new Object();;
 		push_array["id"] = len+i+1;
 		push_array["title"] = classes[i].title;
 		push_array["body"] = classes[i].title;
 		push_array["parent_id"] = null;
-		// push_array["created_at"] = classes[i]["created_at"];
+		push_array["created_at"] = classes[i]["created_at"];
 		push_array["agreement"] = false;
 		push_array["claster"] = classes[i]["claster_id"];
 		change_array.push(push_array);
 		class_array[classes[i]["claster_id"]] = len+i+1;
-		console.log("psuh")
-		console.log(push_array)
 	}
 
 
-	for (var i = 0; i < len; i++){
-		if(dataAll[i]["parent_id"]==null){
-			change_array[i]["parent_id"] = class_array[dataAll[i]["claster"]];
+	for (var i = 0; i < datalen; i++){
+		if(change_array[i]["parent_id"]==null){
+			change_array[i]["parent_id"] = class_array[change_array[i]["claster"]];
 		}
 	}
-	console.log(dataAll);
-	console.log(change_array);
 
 	return change_array;
 
@@ -44,6 +40,17 @@ function tree_data_change(dataAll,classes){
                 return classes[i]["title"]
             }
         }
+    }
+
+    function max_id(array){
+    	var id = 1;
+    	for (var i = 0; i < array.length; i++){
+    		if(id < array[i]["id"]){
+    			id = array[i]["id"];
+    		}
+
+    	}
+    	return id;
     }
 }
 
