ce72b2a58105e69b9cf7bb466ae66a64423dd6ed
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-07-22 07:41:35 +0900

    議論画面の修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :entry_thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [150, 150]
   end
 
   version :user_thumb do
