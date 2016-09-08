beacc0ce481ffe9779a11ff8083fce8bb867d678
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-09-08 16:46:09 +1000

    heroku redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
 require 'redis'
-Redis.current = Redis.new(host: '127.0.0.1', port: 6379)
+if ENV['HEROKU_ENV']
+	Redis.current = Redis.new(host: 'ec2-184-73-182-114.compute-1.amazonaws.com', port: 15529)
+else
+	Redis.current = Redis.new(host: '127.0.0.1', port: 6379)
+end
