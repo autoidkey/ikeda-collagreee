90de0b0ed36684a2a0abbddb87549b7e16e27c33
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-09-19 11:13:14 +1000

    いいねランキング機能の実装

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -674,6 +674,7 @@ class ThemesController < ApplicationController
   def set_ranking
     @users_entry = @theme.joins.includes(:user).map(&:user).sort_by { |u| -u.entries.where(theme_id: @theme, facilitation: false).count }
     @user_ranking = @theme.point_ranking
+    @entry_like_ranking = @theme.like_ranking
     @user_ranking_before_0130 = @theme.point_ranking_before_0130
     @user_ranking_after_0130 = @theme.point_ranking_after_0130
   end
