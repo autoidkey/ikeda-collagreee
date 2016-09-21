9f758d8c10b86cb50622a9cffa49132121e33aaa
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-09-21 22:27:08 +1000

    ICAの本番用

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
+  # validates :name, :realname, presence: true
   validates :name, :realname, presence: true
 
   has_many :entries
