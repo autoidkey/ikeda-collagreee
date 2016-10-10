af93922d7a96171dc9d296098b30e614ff8229b1
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-10-10 13:46:50 +0900

    実験に向けて修正

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -84,7 +84,7 @@ class ThemesController < ApplicationController
       @tree_type = @tree_type[0][:phase_id]
       # 合意フェイズの最初に投票画面に遷移する
       if @tree_type == 3 && !VoteEntry.where(user_id: current_user.id, theme_id: @theme.id).exists?
-        if current_user.home.to_i > 1
+        if current_user.home.to_i < 2
           redirect_to vote_entry_path(@theme.id)
         end
       end
