8def1b4cde54e05a786a224a311a310688384a83
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-12-08 11:49:10 +0900

    バージョンを戻す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,8 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << col
     end
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    root_path || user_admin_path
+  end
 end
