522c2531597e61fb7f3b79e590e04c24e631e433
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2016-12-17 20:29:42 +0900

    少し修正

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -4,4 +4,7 @@ module ThemesHelper
 	def_erb_method('render_panel(entry, count_entry, show_entry )', "#{Rails.root}/app/views/themes/_panel.html.erb")
 
 	def_erb_method('render_entry(entry, count_entry, show_entry )', "#{Rails.root}/app/views/themes/_entry.html.erb")
+
+	def_erb_method('render_modal_form(child)', "#{Rails.root}/app/views/themes/_modal_form.html.erb")
+
 end
