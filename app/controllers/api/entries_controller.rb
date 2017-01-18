22c4cb9032df85aa5830e4c3ee088edc948a5d2e
Akihisa <sengoku.akihisa@itolab.nitech.ac.jp>
2017-01-18 19:02:18 +0900

    要約機能の削除、更新を確認機能の修正

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -3,11 +3,15 @@ module Api
 
     def edit
       @entries = Entry.where(theme_id: params[:id])
+      p params[:time]
       render json: @entries.count
     end
 
     def show
       @entries = Entry.where(theme_id: params[:id])
+      @entries = Entry.where("created_at > ? and theme_id = ? and user_id != ?", params[:time], params[:id], current_user.id)
+      p params[:time]
+      p @entries.count
       render json: @entries.count
     end
 
