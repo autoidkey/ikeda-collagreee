c27ab229ea2a2f2a0be52db5079150e2546358a9
akihia.sengoku <akihisa1788@yahoo.co.jp>
2017-06-26 12:00:25 +0900

    fix experiment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-10.times do |i|
+8.times do |i|
 	6.times do |t|
 		num = 10* i + t
 		array = [t+1,i+1]
 		flag = true
-		if ![[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[2,9],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[3,9],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7],[4,8],[4,9]].include?(array)
-			p array
+		if ![[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7]].include?(array)
   			User.create(:name => "user#{num}",realname: "user#{num}", email: "test#{num}@test.co.jp", password: "123456", role: 2, row: i+1, col: t+1)
   		end
 	end
+end
+
+3.times do |i|
+	User.create(:name => "admin#{num}",realname: "admin#{num}", email: "admin#{num}@admin.co.jp", password: "123456", role: 0)
 end
