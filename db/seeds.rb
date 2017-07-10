e54fa3db6d6c45e3c6d7751e4687402ba2d440d0
akihia.sengoku <akihisa1788@yahoo.co.jp>
2017-07-10 08:12:23 +0800

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,41 +19,41 @@ require 'roo'
 # 	end
 # end
 
-ods = Roo::Spreadsheet.open('db/idpass1.xlsx')
-ods.each_with_index do |sheet, i|
-  	name =  sheet[0]
-  	email = sheet[1]
-  	pass = sheet[2]
-    array = [[9,4],[9,5],[10,5],[11,5],[9,6],[10,6],[11,6],[3,4],[3,5],[3,6]]
-  	row = i /11 + 1
-  	col = i % 11 + 1
-  	if col != 4 && col != 8 && row < 7
-      if !array.include?([col,row])
-  		  User.create(:name => name,realname: name, email: email, password: pass, role: 2, row: col, col: row)
-      else
-        User.create(:name => name,realname: name, email: email, password: pass, role: 2)
-      end
-  	else
-      User.create(:name => name,realname: name, email: email, password: pass, role: 2)
-    end
-end
+# ods = Roo::Spreadsheet.open('db/idpass1.xlsx')
+# ods.each_with_index do |sheet, i|
+#   	name =  sheet[0]
+#   	email = sheet[1]
+#   	pass = sheet[2]
+#     array = [[9,4],[9,5],[10,5],[11,5],[9,6],[10,6],[11,6],[3,4],[3,5],[3,6]]
+#   	row = i /11 + 1
+#   	col = i % 11 + 1
+#   	if col != 4 && col != 8 && row < 7
+#       if !array.include?([col,row])
+#   		  User.create(:name => name,realname: name, email: email, password: pass, role: 2, row: col, col: row)
+#       else
+#         User.create(:name => name,realname: name, email: email, password: pass, role: 2)
+#       end
+#   	else
+#       User.create(:name => name,realname: name, email: email, password: pass, role: 2)
+#     end
+# end
 
-ods = Roo::Spreadsheet.open('db/idpass1.xlsx')
-ods.each do |sheet|
-  	name =  sheet[0]
-  	email = sheet[1]
-  	pass = sheet[2]
-  	User.create(:name => name,realname: name, email: email, password: pass, role: 2)
-end
+# ods = Roo::Spreadsheet.open('db/idpass1.xlsx')
+# ods.each do |sheet|
+#   	name =  sheet[0]
+#   	email = sheet[1]
+#   	pass = sheet[2]
+#   	User.create(:name => name,realname: name, email: email, password: pass, role: 2)
+# end
 
 3.times do |i|
 	num = i + 1
 	User.create(:name => "admin#{num}",realname: "admin#{num}", email: "admin#{num}@admin.co.jp", password: "123456", role: 0)
 end
 
-User.create(:name => "Prof. Ping Zhong Tong",realname: "なし", email: "ping@collagree.com", password: "123456", role: 2)
-User.create(:name => "Prof. Shigeo Matsubara",realname: "なし", email: "shigeo@collagree.com", password: "123456", role: 2)
-User.create(:name => "Prof. Matthew Taylor",realname: "なし", email: "matthew@collagree.com", password: "123456", role: 2)
-User.create(:name => "Prof. Takayuki Ito",realname: "なし", email: "takayuki@collagree.com", password: "123456", role: 2)
-User.create(:name => "Prof. Minjie Zhang",realname: "なし", email: "minjie@collagree.com", password: "123456", role: 0)
+# User.create(:name => "Prof. Ping Zhong Tong",realname: "なし", email: "ping@collagree.com", password: "123456", role: 2)
+# User.create(:name => "Prof. Shigeo Matsubara",realname: "なし", email: "shigeo@collagree.com", password: "123456", role: 2)
+# User.create(:name => "Prof. Matthew Taylor",realname: "なし", email: "matthew@collagree.com", password: "123456", role: 2)
+# User.create(:name => "Prof. Takayuki Ito",realname: "なし", email: "takayuki@collagree.com", password: "123456", role: 2)
+# User.create(:name => "Prof. Minjie Zhang",realname: "なし", email: "minjie@collagree.com", password: "123456", role: 0)
 
